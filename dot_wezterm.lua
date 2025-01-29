c37fe102e85d8dd427a1167a8122fb4225041268
Marcelo Belli <marcelo@belli.me>
2025-01-29 16:02:39 -0300

    Brewfile cleanup; ghostty

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Latte"
+config.color_scheme = "Catppuccin Frappe"
 
 config.font = wezterm.font("Monolisa")
 config.font_size = 13.5
